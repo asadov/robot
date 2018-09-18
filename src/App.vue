aacb085affcb175c50bdcebbea4522b2630944fc
asadov <salihasadov@gmail.com>
2018-09-19 01:36:24 +0300

    added owners name

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,25 @@
         <ul>
           <li class="nav-item">
             <img class="logo" src="./assets/build-a-bot-logo.png"/>
-            Build-a-Bot with salih
+            Build-a-Bot
           </li>
         </ul>
       </nav>
     </header>
     <main>
-    <HomePage/>
+    <robot-builder/>
     </main>
   </div>
 </template>
 
 <script>
-import HomePage from './home/HomePage.vue';
+   //import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'app',
   components: {
-    HomePage,
+    RobotBuilder,
   },
 };
 </script>
